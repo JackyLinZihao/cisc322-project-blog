88ad3469e35377f4cefe40272ea95b0503bc8ce9
JackyLinZihao <139222474+JackyLinZihao@users.noreply.github.com>
2024-10-28 17:35:50 -0400

    Fix publicPath in vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,17 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  publicPath: '/cisc322-project-blog/' 
 })
-module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/cisc322-project-blog/'
-    : '/'
-}
 
-module.exports = {
-  productionSourceMap: false,
-  css: {
-    extract: true,
-    sourceMap: false,
-  },
-};
+
